8bdc2a639838cfb6f31ba6a2c2c32561d662ea2d
kingmoc <joe.c.a.garcia@gmail.com>
2019-05-17 14:20:53 -0500

    Everthing Complete

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -4,6 +4,9 @@ class Carousel {
 
 let carousel = document.querySelector();
 
+
+
+
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
     2. You will need to grab a reference to all of the images
